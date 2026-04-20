caf0e2ccb5c3ab7173a7c1a735aa6a1a188caee2
paola <danapof307@gmail.com>
2026-04-20 00:41:36 -0600

    Creación del proyecto y prueba de la base de datos con la creación de la tabla productos.

---
+++ resources/views/Inicio.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Papaleria</h1>
+
+    <button onclick="window.location='{{ route('productos.index') }}'">
+    Productos
+</button>
+     <a href="{{route('productos.index') }}">Productos</a>
+     
+    
+
+</body>
+</html>

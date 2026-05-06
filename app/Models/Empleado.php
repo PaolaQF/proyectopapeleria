b71635183946d37e4598319182ff41f4877d109a
Admin <miab4311@gmail.com>
2026-05-05 23:03:14 -0600

    Actualizacion de modelos empleado

--- app/Models/Empleado.php
+++ app/Models/Empleado.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+
 class Empleado extends Model
 {
     use HasFactory;
@@ -12,18 +13,4 @@ class Empleado extends Model
     protected $table = 'empleados';
     protected $primaryKey = 'id';
     public $timestamps = false;
-
-    protected $fillable = [
-        'sucursal_id',
-        'nombre',
-        'apellido_paterno',
-        'apellido_materno',
-        'puesto',
-        'telefono',
-        'email',
-        'fecha_ingreso',
-        'salario',
-        'turno',
-        'estatus'
-    ];
 }
